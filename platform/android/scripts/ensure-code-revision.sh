6a4ef02e6ac5e4f0e6b61a372770d6db19bb8618
Helium314 <helium314@mailbox.org>
2023-03-26 07:46:03 +0200

    does it work now?

--- platform/android/scripts/ensure-code-revision.sh
+++ platform/android/scripts/ensure-code-revision.sh
@@ -8,18 +8,6 @@ function step { >&2 echo -e "\033[1m\033[36m* $@\033[0m"; }
 function finish { >&2 echo -en "\033[0m"; }
 trap finish EXIT
 
-VERSION_TAG=${VERSION_TAG:-''}
-if [[ -z ${VERSION_TAG} ]]; then
-    step "Determining version number from most recent relevant git tag…"
-    VERSION_TAG=$( git describe --tags --match=android-v*.*.* --abbrev=0 )
-    echo "Found tag: ${VERSION_TAG}"
-fi
-
-if [[ $( echo ${VERSION_TAG} | grep --invert-match android-v ) ]]; then
-    echo "Error: ${VERSION_TAG} is not a valid android version tag"
-    echo "VERSION_TAG should be in format: android-vX.X.X-pre.X"
-    exit 1
-fi
-
+VERSION_TAG=android-v10.0.2-pre.1
 step "Checking out source code at ${VERSION_TAG}"
 git checkout ${VERSION_TAG}
